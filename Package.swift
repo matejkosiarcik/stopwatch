2169c94e8f7055e5a2ca02d2ac1d3a12bee1075f
Matej Košiarčik <matej@binarytrex.com>
2017-06-25 23:27:11 +0200

    [CONF] Add test dependency

--- Package.swift
+++ Package.swift
@@ -4,8 +4,12 @@ import PackageDescription
 
 let package = Package(name: "StopWatch")
 
+package.dependencies = [
+    .package(url: "https://github.com/Quick/Nimble.git", from: "7.0.0")
+]
+
 package.targets = [
     .target(name: "swatch", dependencies: ["StopWatch"]), // executable
     .target(name: "StopWatch", dependencies: []), // library
-    .testTarget(name: "StopWatchTests", dependencies: ["StopWatch"]),
+    .testTarget(name: "StopWatchTests", dependencies: ["StopWatch", "Nimble"]),
 ]
