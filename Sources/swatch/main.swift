abeb3a67b620a0e3d91701b3c6b1f2f25efe07b2
Matej Košiarčik <matej@binarytrex.com>
2017-06-25 23:28:26 +0200

    [ADD] Make basic main demostrating reading unbuffered from input

--- Sources/swatch/main.swift
+++ Sources/swatch/main.swift
@@ -1,3 +1,35 @@
+//
+// main.swift
+// Copyright © 2017 Matej Kosiarcik. All rights reserved.
+//
+import Foundation
 import StopWatch
 
-print(foo())
+// setup unbuffered standard input
+setbuf(stdin, nil)
+setvbuf(stdin, nil, _IONBF, 0)
+var input = termios()
+tcgetattr(STDIN_FILENO, &input)
+input.c_lflag = tcflag_t(Int32(input.c_lflag) & ~ICANON)
+tcsetattr(STDIN_FILENO, TCSANOW, &input)
+
+func readCharacter(from file: FileHandle) -> Character? {
+    let data = file.readData(ofLength: 1)
+    let string = String(data: data, encoding: .ascii)
+    return string?.characters.first
+}
+
+func demo() {
+    let str = readCharacter(from: .standardInput).map { String($0) } ?? ""
+    print("\r", terminator: "")
+    switch str {
+    case "\n": print("newline")
+    case " ": print("space")
+    case "a": print("A it is.")
+    default: break
+    }
+}
+
+while true {
+    demo()
+}
