5aa4ec9c84060ed4d323f99d33396955c0c5eb45
Matej Košiarčik <matej@binarytrex.com>
2017-06-28 11:24:31 +0200

    [REF] Move character reading function to library

---
+++ Sources/StopWatch/Reading.swift
@@ -0,0 +1,12 @@
+//
+// Shell.swift
+// Copyright © 2017 Matej Kosiarcik. All rights reserved.
+//
+
+import Foundation
+
+public func readCharacter(from file: FileHandle) -> Character? {
+    let data = file.readData(ofLength: 1)
+    let string = String(data: data, encoding: .ascii)
+    return string?.characters.first
+}
---
+++ Tests/StopWatchTests/ReadingTests.swift
@@ -0,0 +1,25 @@
+//
+// ReadingTests.swift
+// Copyright © 2017 Matej Kosiarcik. All rights reserved.
+//
+
+import Nimble
+import XCTest
+@testable import StopWatch
+
+final class ReadingTest: XCTestCase {}
+
+extension ReadingTest {
+    func testReadingSingleCharacter() {
+        // given
+        let filePath = NSTemporaryDirectory().appending("/StopWatch.txt.tmp")
+        FileManager.default.createFile(atPath: filePath, contents: "abc".data(using: .ascii))
+
+        // when
+        guard let reader = FileHandle(forReadingAtPath: filePath) else { XCTFail(); return }
+        let char = readCharacter(from: reader)
+
+        // then
+        expect(char) == Character("a")
+    }
+}
