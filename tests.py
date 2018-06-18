3f99d67b1ffe757350d12603709587c8bad12853
Matej Košiarčik <matejkosiarcik@gmail.com>
2018-06-18 11:36:53 +0200

    [TEST] Make test time-deltas bigger
    This should make (travis) failing less frequent

--- tests.py
+++ tests.py
@@ -33,30 +33,30 @@ class TimerTests(unittest.TestCase):
     def test_timer_basic(self):
         def operations(timer):
             timer.start()
-            time.sleep(0.2)
+            time.sleep(0.3)
             timer.stop()
 
         data = timer_execute(operations)
 
         self.assertEqual(data['lap'], 1)
-        self.assertAlmostEqual(data['time_relative'], 0.2, delta=0.15)
-        self.assertAlmostEqual(data['time_absolute'], 0.2, delta=0.15)
+        self.assertAlmostEqual(data['time_relative'], 0.3, delta=0.25)
+        self.assertAlmostEqual(data['time_absolute'], 0.3, delta=0.25)
 
     def test_timer_pausing(self):
         def operations(timer):
             timer.start()
-            time.sleep(0.2)
+            time.sleep(0.3)
             timer.stop()
-            time.sleep(0.2)
+            time.sleep(0.5)
             timer.start()
-            time.sleep(0.2)
+            time.sleep(0.3)
             timer.stop()
 
         data = timer_execute(operations)
 
         self.assertEqual(data['lap'], 1)
-        self.assertAlmostEqual(data['time_relative'], 0.4, delta=0.15)
-        self.assertAlmostEqual(data['time_absolute'], 0.4, delta=0.15)
+        self.assertAlmostEqual(data['time_relative'], 0.6, delta=0.25)
+        self.assertAlmostEqual(data['time_absolute'], 0.6, delta=0.25)
 
     def test_timer_lapping_simple(self):
         def operations(timer):
@@ -73,18 +73,18 @@ class TimerTests(unittest.TestCase):
     def test_timer_lapping_complex(self):
         def operations(timer):
             timer.start()
-            time.sleep(0.2)
+            time.sleep(0.3)
             timer.stop()
             timer.lap()
             timer.start()
-            time.sleep(0.2)
+            time.sleep(0.3)
             timer.stop()
 
         data = timer_execute(operations)
 
         self.assertEqual(data['lap'], 2)
-        self.assertAlmostEqual(data['time_relative'], 0.2, delta=0.15)
-        self.assertAlmostEqual(data['time_absolute'], 0.4, delta=0.15)
+        self.assertAlmostEqual(data['time_relative'], 0.3, delta=0.25)
+        self.assertAlmostEqual(data['time_absolute'], 0.6, delta=0.25)
 
 
 def timer_execute(operations):
